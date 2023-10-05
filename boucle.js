a2a72c7cb3a1ae1c9a01bfc527c2ca7156c54278
pchloe02 <pelerinchloe@gmail.com>
2023-10-05 16:15:42 +0200

    boucle

--- boucle.js
+++ boucle.js
@@ -1,1 +1,23 @@
 
+
+
+// function printName(fruit, legume){
+//     console.log(`hello ${fruit}, bye ${legume}`)
+// }
+
+// console.log(printName("poire", "haricot"))
+
+fruits= ["banana", "apple", "pear", "fraise", "myrtille"]
+
+//console.log(fruits.length)
+
+ for(i=0 ; i< fruits.length ; i++){
+console.log( "Quand i est égale à :" + i ,fruits[i])
+ }
+
+ //while(i< fruits.length) {
+//  console.log( "Quand i est égale à :" + i ,fruits[i])
+    
+ //}
+
+ 
